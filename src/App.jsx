829265648b44f1da2f6c8028aec17add36c63400
Salman Izhar <Sbinazhar671@gmail.com>
2024-02-02 22:40:33 +0600

    videoPlayer

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
-import VideoPlayer from "./components/VideoPlayer";
 
 function App() {
-  const [isPlaying, setPlaying] = useState(false);
-  useEffect(() => {
-    // some code
-  });
-  return (
-    <>
-      <button onClick={() => setPlaying(!isPlaying)}>
-        {isPlaying ? "Pause" : "Play"}
-      </button>
-      <VideoPlayer
-        isPlaying={isPlaying}
-        src={`https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4`}
-      />
-    </>
-  );
+  useEffect(() => {});
+  return <></>;
 }
 
 export default App;
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,19 +1,28 @@
 import { useEffect, useRef } from "react";
 
 export default function VideoPlayer({ isPlaying, src }) {
-  const ref = useRef();
+  const ref = useRef(null);
 
-  console.dir(ref.current);
+  //   const [counter, setCounter] = useState(0);
+  //   its bad practice. because its contains loop.
+  //   useEffect(() => {
+  //     setCounter(counter + 1);
+  //   });
+  //   return (
+
   useEffect(() => {
     if (isPlaying) {
+      console.log("Calling video.play()");
       ref.current.play();
     } else {
+      console.log("Calling video.pause()");
       ref.current.pause();
     }
-  });
+  }, [isPlaying]);
+
   return (
     <>
-      <video ref={ref} src={src}></video>
+      <video ref={ref} src={src} loop playsInline></video>
     </>
   );
 }
