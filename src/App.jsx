d7f5d0eaa1e4ff688d928ceb01a5d667131bcd17
Salman Izhar <Sbinazhar671@gmail.com>
2024-02-05 21:46:54 +0600

    unnecessaryEffectPart2

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
-// import ExampleOne from "./components/ExampleOne";
-import { useState } from "react";
-import ExampleThree from "./components/ExampleThree";
-import { items1, items2 } from "./data/data";
-// import ExampleTwo from "./components/ExampleTwo";
+import "./App.css";
+import Example6 from "./components/Example6";
 
-function App() {
-  // const [userId, setUserId] = useState();
-  const [items, setItems] = useState(items1);
+export default function App() {
   return (
-    <>
-      {/* ExampleOne: Updating state based on props or state */}
-      {/* <ExampleOne /> */}
-
-      {/* ExampleTwo: Resetting all state when a prop changes */}
-      {/* <button onClick={() => setUserId(userId === 1 ? 2 : 1)}>
-        Switch To Profile NO: {userId === 1 ? 2 : 1}
-      </button>
-      <ExampleTwo key={userId} userId={userId} /> */}
-
-      {/* ExampleThree: Adjusting some state when a prop changes */}
-      <button onClick={() => setItems(items1)}>Switch To Item1</button>
-      <button onClick={() => setItems(items2)}>Switch To Item2</button>
-      <ExampleThree items={items} />
-    </>
+    <div>
+      {/* Sharing logic between event handlers */}
+      {/* <Example4 /> */}
+      {/* <Example5 /> */}
+      <Example6 />
+    </div>
   );
 }
-
-export default App;
