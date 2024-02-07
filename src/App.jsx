5473e118887c98b8e1b72fe2efb1e883228a5271
Salman Izhar <Sbinazhar671@gmail.com>
2024-02-07 17:59:04 +0600

    EffectEventPointerMove

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,5 @@
-import { useState } from "react";
-import Page from "./components/Page";
-import { shoppingCardContext } from "./context/ShoppingCardContext";
-
-const items = [
-  {
-    id: 1,
-    title: "Product 1",
-  },
-];
+import Pointer from "./components/Pointer";
 
 export default function App() {
-  const [page, setPage] = useState("/home");
-  const [cardItem, setCardItem] = useState(items);
-
-  function handleAddItem() {
-    return setCardItem([
-      ...cardItem,
-      {
-        id: 2,
-        title: "ami tomay balobasi",
-      },
-    ]);
-  }
-
-  const handlePageChange = () => {
-    setPage("/card");
-  };
-  return (
-    <div>
-      <shoppingCardContext.Provider value={cardItem}>
-        <Page
-          url={page}
-          onPageChange={handlePageChange}
-          addItem={handleAddItem}
-        />
-      </shoppingCardContext.Provider>
-    </div>
-  );
+  return <Pointer />;
 }
