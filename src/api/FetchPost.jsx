c0009c7b4ca5223d8696b0f9f579dd5625dc5bb4
Salman Izhar <Sbinazhar671@gmail.com>
2024-03-06 07:17:40 +0600

    5.8

--- src/api/FetchPost.jsx
+++
@@ -1,6 +0,0 @@
-import wrapPromise from "../utils/wrapPromise";
-
-export default function FetchPost(url) {
-  const response = fetch(url).then((res) => res.json());
-  return wrapPromise(response);
-}
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBW7QLf-_df3JNFXGKeoGpD0ndPW_TeMnI",
+  authDomain: "guestbook-d280a.firebaseapp.com",
+  projectId: "guestbook-d280a",
+  storageBucket: "guestbook-d280a.appspot.com",
+  messagingSenderId: "451888273325",
+  appId: "1:451888273325:web:ce19e2a41b0e727948f324",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
